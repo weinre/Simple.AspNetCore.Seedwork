852c802bb1c7227e50d1df0aecc6a0ae574a8a03
牛排大叔 <beefsteak@live.cn>
2018-08-10 18:46:01 +0800

    生成nuget版本号

---
+++ src/AspNetCore.WebApi.Seedwork/AspNetCore.WebApi.Seedwork.Log/Extensions/LogServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿/* ***********************************************
+ * author:  奔跑的牛排
+ * email:   beefsteak@live.cn  
+ * ***********************************************/
+
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using NLog.Extensions.Logging;
+using NLog.Web;
+
+namespace AspNetCore.WebApi.Seedwork.Extensions
+{
+    public static class LogServiceCollectionExtensions
+    {
+        public static void AddSimpleNLog(this IServiceCollection services, IConfiguration configuration, string defaultLoggingConfigKey = "Logging")
+        {
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddConfiguration(configuration.GetSection(defaultLoggingConfigKey));
+            });
+        }
+
+        public static void UseSimpleNLog(this ILoggerFactory loggerFactory, IHostingEnvironment env, string defaultLogConfigFileName = "nlog.config")
+        {
+            loggerFactory.AddNLog();
+            var contentRootPath = env.ContentRootPath;
+            var applicationBasePath = PlatformServices.Default.Application.ApplicationBasePath;
+            var logFileName = Path.Combine(applicationBasePath.Replace(contentRootPath, ""), defaultLogConfigFileName);
+            logFileName = logFileName.IndexOf('\\') >= 0 ? logFileName.Substring(1, logFileName.Length - 1) : logFileName;
+            env.ConfigureNLog(logFileName);
+        }
+    }
+}
